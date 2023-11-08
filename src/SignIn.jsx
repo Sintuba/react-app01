f872cc08bfd30d5771f3b957953c54e42056945f
sintuba <sintuba@gmail.com>
2023-11-08 09:49:08 +0900

    11/8

---
+++ src/SignIn.jsx
@@ -0,0 +1,16 @@
+
+const SignIn = () =>{
+    return(
+        <>
+            <div className="sighnIn">
+                <div className="signIn__wrapper">
+                    <div className="signIn__form">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SignIn;
