2d55a17fd8d802cf08c149e88ed03ebc3533ef78
Sintuba <sintuba@gmail.com>
2023-11-12 23:38:54 +0900

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter  } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Default from "./pages/Default";
